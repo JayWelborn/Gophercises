5d2a911837eebefe2b97ea87ff0cfe93da4ae276
jaywelborn <jesse.welborn@gmail.com>
2019-09-22 16:57:10 -0400

    trimmed whitespace from csv answers

--- Exercise1/part1.go
+++ Exercise1/part1.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type problem struct {
@@ -46,7 +47,8 @@ func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 	for i, line := range lines {
 		ret[i] = problem{
-			line[0], line[1],
+			line[0],
+			strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
